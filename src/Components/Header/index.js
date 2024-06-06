bc7f21b77104be2c5d1b997bb18119efeb835103
Dienay Lima <dienaylima@gmail.com>
2024-06-06 13:46:23 -0300

    feature: create header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import {
+  CartButton,
+  CartCounter,
+  HeaderContainer,
+  LogoButton,
+  SingUpLink,
+  UserUtilities,
+} from './styles';
+import Logo from '../Logo';
+import { useNavigate } from 'react-router-dom';
+import Cart from '../../Images/icons/cart.svg';
+import Dropdown from '../Dropdown';
+
+function Header({ orderLength = 0 }) {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [cartCounter, setCartCounter] = useState(0);
+
+  const token = window.localStorage.getItem('token');
+
+  useEffect(() => {
+    if (token === null) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+    setCartCounter(orderLength);
+  }, [orderLength, token]);
+
+  return (
+    <HeaderContainer>
+      <LogoButton onClick={() => navigate('/home')}>
+        <Logo variant="small" />
+      </LogoButton>
+      <UserUtilities>
+        {isLoggedIn ? (
+          <Dropdown />
+        ) : (
+          <SingUpLink onClick={() => navigate('/login')}>Entrar</SingUpLink>
+        )}
+
+        <CartButton>
+          <img src={Cart} alt="Carrinho" />
+          {cartCounter === 0 ? <></> : <CartCounter>{cartCounter}</CartCounter>}
+        </CartButton>
+      </UserUtilities>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
