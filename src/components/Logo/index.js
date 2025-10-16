4e3111981daf749c42a61354a7cbfeec5de7746a
Dienay Lima <dienaylima@gmail.com>
2025-10-16 11:02:29 -0300

    feat: update Logo component to link to home page using React Router Link and styled component

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,13 @@
-import React from "react";
 import { LogoStyle } from "./style";
 import LogoImage from "../../Images/logo-rangos.svg";
+import { Link } from "react-router-dom";
 
 function Logo({ $variant = "normal" }) {
-  return <LogoStyle src={LogoImage} alt="Logo Rappi4" $variant={$variant} />;
+  return (
+    <Link to="/">
+      <LogoStyle src={LogoImage} alt="Logo Rappi4" $variant={$variant} />
+    </Link>
+  );
 }
 
 export default Logo;
