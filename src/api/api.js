702a5ef417cdc28e80487f94f2cffe6db13bde90
Dienay Lima <dienaylima@gmail.com>
2025-10-16 09:39:40 -0300

    fix: correct env.API_URL access in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { env } from "../utils";
 
-const { API_URL } = env;
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: env.API_URL,
 });
 
 export default api;
