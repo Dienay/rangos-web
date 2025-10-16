5ed1a25896cbcd941ca36aa2fc4776e608b2eec8
Dienay Lima <dienaylima@gmail.com>
2025-10-16 09:41:56 -0300

    feat: add getTopProducts API method and refactor CardEstablishment to use new util and styling props

--- src/api/productService.js
+++ src/api/productService.js
@@ -4,3 +4,8 @@ export const getProducts = async () => {
   const response = await api.get("/products");
   return response.data.products;
 };
+
+export const getTopProducts = async () => {
+  const response = await api.get("/products/top");
+  return response.data.topProducts;
+};
